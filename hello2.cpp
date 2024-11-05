72fbd34d28e9f76af5511ea730b3f535044a5888
OmAnand857 <allansmith2561@gmail.com>
2024-11-05 15:34:52 +0530

    commit from loca_repo2

---
+++ hello2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	cout<<"Hello World, I have modified it just now!"<<endl;
+	cout<<"Pushing from another local repo"<<endl;
+	return 0;
+
+}
